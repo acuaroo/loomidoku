b0e2d21cc3d882e82c19817f88b6bd0a023b74c4
Acuaro <67112172+acuaroo@users.noreply.github.com>
2023-12-14 08:59:55 -0700

    Added the beginning of categories

--- server/src/scraper.js
+++ server/src/scraper.js
@@ -2,20 +2,33 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const fs = require("fs").promises;
 const path = require("path");
+const cliProgress = require('cli-progress');
 
 const filePath = path.join(__dirname, "../ldata/loomians.json");
 const url = "https://loomian-legacy.fandom.com/wiki/Loomian";
 
+const banPhrases = [
+    "Loomians",
+    "Typeless",
+    "type",
+    "Pages with broken file links",
+    "Work in Progress",
+    "Loomians with Variants",
+    "Trainer Mastery",
+]
+
+let uniqueCategories = new Set();
+
 const fetchData = async () => {
     try {
         const response = await axios.get(url);
-    return cheerio.load(response.data);
+        return cheerio.load(response.data);
     } catch (error) {
         throw new Error(`err fetching data: ${error.message}`);
     }
 };
 
-const processTableRow = ($, row) => {
+const processTableRow = async ($, row, progressBar) => {
     const [idCell, imageCell, typeCell] = $(row).find("td").toArray();
 
     const loomian = {
@@ -23,17 +36,50 @@ const processTableRow = ($, row) => {
         name: $(imageCell).find("img").attr("alt") || "",
         image: $(imageCell).find("img").attr("data-src") || $(imageCell).find("img").attr("src") || "",
         type: $(typeCell).find("a span").text().replace(/\n/g, "") || "",
+        categories: [],
     };
 
     loomian.name = loomian.name.replace(/-menu$/, '');
     loomian.type = loomian.type.replace(/([a-z])([A-Z])/g, '$1:$2');
 
+    loomian.categories = await processLoomianPage(
+        `https://loomian-legacy.fandom.com/wiki/${loomian.name}`
+    );
+
+    progressBar.increment();
+    
     return loomian;
 };
 
-const processTable = ($) => {
-    const loomians = $(".wikitable tbody tr").map((_, row) => processTableRow($, row)).get();
+const processTable = async ($, progressBar) => {
+    const loomians = await Promise.all(
+        $(".wikitable tbody tr").map(async (_, row) => await processTableRow($, row, progressBar)).get()
+    );
     return loomians.filter(({ id, name, image, type }) => id && name && image && type);
+};
+
+const processLoomianPage = async (loomianPageUrl) => {
+    try {
+        const response = await axios.get(loomianPageUrl);
+        const $ = cheerio.load(response.data);
+
+        const categoryLinks = $("div.page-header__categories a[title^='Category:']").map((_, elem) => $(elem).text()).get();
+        let categories = categoryLinks.map(link => link.replace("Category:", ""));
+
+        // filter the array using the ban phrases
+        categories = categories.filter(category => !banPhrases.includes(category));
+        
+        categories.forEach(category => {
+            if (!uniqueCategories.has(category)) {
+                uniqueCategories.add(category);
+            }
+        });
+        
+        return categories;
+    } catch (error) {
+        console.error(`Error processing Loomian page ${loomianPageUrl}: ${error.message}`);
+        return [];
+    }
 };
 
 const saveToFile = async (data) => {
@@ -48,7 +94,17 @@ const saveToFile = async (data) => {
 const main = async () => {
     try {
         const $ = await fetchData();
-        const loomians = processTable($);
+        const totalRows = $(".wikitable tbody tr").length;
+
+        const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+        progressBar.start(totalRows, 0);
+
+        const loomians = await processTable($, progressBar);
+
+        progressBar.stop();
+
+        console.log(uniqueCategories);
+
         await saveToFile(loomians);
     } catch (error) {
         console.error(error.message);
