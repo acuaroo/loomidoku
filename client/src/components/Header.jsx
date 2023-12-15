b87f99495c187422506b0610b57d5bd8869e4bfa
acuaroo <milan.m.nair@gmail.com>
2023-12-15 14:35:30 -0700

    Messed with styling + Added icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,22 @@
+import { HiOutlineExternalLink, HiOutlineQuestionMarkCircle } from "react-icons/hi";
+
 function Header() {
   return (
     <>
-      <div className="bg-zinc-700 h-[10%] w-screen flex justify-between">
-        <div className="flex h-full items-center p-10">
-          <h1 className="font-russo text-white text-4xl">Loomidoku</h1>
+      <div className="font-russo text-white bg-zinc-700 h-[10%] w-screen flex justify-between">
+        <div className="flex h-full items-center p-8">
+          <h1 className="text-xl sm:text-2xl md:text-4xl">Testing</h1>
         </div>
 
-        <div className="flex h-full items-center p-10">
-          <div className="bg-zinc-800 rounded-lg h-10 w-10 text-center">
-            ?
+        <div className="flex h-full items-center p-8 text-xl">
+          <div className="p-4 m-2 bg-zinc-800 rounded-lg text-center">
+            <HiOutlineQuestionMarkCircle className="text-2xl"/>
+          </div>
+          <div className="p-4 m-2 bg-red-500 rounded-lg text-center">
+            <HiOutlineExternalLink />
+          </div>
+          <div className="p-4 m-2 bg-cyan-400 rounded-lg text-center">
+            <HiOutlineExternalLink/>
           </div>
         </div>
       </div>
